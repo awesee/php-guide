bf021c9b7b5dae8a10f57fa62b860c0fc34309d4
openset <openset.wang@gmail.com>
2019-03-22 18:26:06 +0800

    Add: quickSortV2

--- Example/quick_sort.php
+++ Example/quick_sort.php
@@ -29,4 +29,33 @@ function quickSort(&$arr, $l, $r)
     }
 }
 
+function quickSortV2(&$arr, $l, $r)
+{
+    if ($l < $r) {
+        $mid = $arr[$l];
+        $i = $l + 1;
+        $head = $l;
+        $tail = $r;
+        while ($head < $tail) {
+            if ($arr[$i] > $mid) {
+                list($arr[$i], $arr[$tail]) = [$arr[$tail], $arr[$i]];
+                $tail--;
+            } else {
+                list($arr[$i], $arr[$head]) = [$arr[$head], $arr[$i]];
+                $head++;
+                $i++;
+            }
+        }
+        $arr[$head] = $mid;
+        quickSortV2($arr, $l, $head - 1);
+        quickSortV2($arr, $head + 1, $r);
+    }
+}
+
+$arr2 = $arr;
+
 quickSort($arr, 0, count($arr) - 1);
+print_r($arr);
+
+quickSortV2($arr2, 0, count($arr) - 1);
+print_r($arr2);
