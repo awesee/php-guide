52d21592fd19e5889559379bde27de12ebd45db3
Openset <openset.wang@gmail.com>
2022-12-13 12:02:29 +0800

    feat: version

---
+++ Example/version.php
@@ -0,0 +1,19 @@
+<?php
+
+// 版本格式：主版本号.次版本号.修订号
+function version($version, $carry = 1)
+{
+    preg_match('/^v(\d*)\.?(\d*)\.?(\d*)$/', $version, $matches);
+    print_r($matches);
+    if (empty($matches)) {
+        return 'v0.1.0';
+    }
+    for ($i = 3; $i >= 1; $i--) {
+        $matches[$i] = intval($matches[$i]) + $carry;
+        if ($i >= 2) {
+            $carry = intval($matches[$i] / 100);
+            $matches[$i] = $matches[$i] % 100;
+        }
+    }
+    return sprintf('v%d.%d.%d', $matches[1], $matches[2], $matches[3]);
+}
